5dc97c7d9be7e075a75b21bd4d4430faa7373193
Kishan Bhakta <kbzz249@gmail.com>
2021-12-22 23:32:47 -0600

    Create memalloc.c

---
+++ memalloc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/***** MACRO DEFINITION ***/
+  #define ARR_SIZE 1000
+  
+ /***** FUNCTION DECLARATION ***/
+ void f1(void);
+void f2(void);
+void f3(void);
+
+int main(){
